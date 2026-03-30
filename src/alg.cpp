42a56f9360909c8cf54db353a3d7b370d63b6843
saq1rette <165058154+saq1rette@users.noreply.github.com>
2026-03-30 19:58:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -28,23 +28,30 @@ int binarySearch(int *arr, int left, int right, int target) {
   while (left <= right) {
     int mid = left + (right - left) / 2;
     if (arr[mid] == target) {
-      return mid;
+      int leftmost = mid;
+      int rightmost = mid;
+      while (leftmost > left && arr[leftmost - 1] == target) {
+        leftmost--;
+      }
+      while (rightmost < right && arr[rightmost + 1] == target) {
+        rightmost++;
+      }
+      return rightmost - leftmost + 1;
     } else if (arr[mid] < target) {
       left = mid + 1;
     } else {
       right = mid - 1;
     }
   }
-  return -1;
+  return 0;
 }
 
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; i++) {
     int target = value - arr[i];
-    if (binarySearch(arr, i + 1, len - 1, target) != -1) {
-      count++;
-    }
+    int cnt = binarySearch(arr, i + 1, len - 1, target);
+    count += cnt;
   }
   return count;
 }
